5f2d8e8bdb1e7b57ed00435119fb1bf538e1262d
Dardanboy <dardan.iljazi@gmail.com>
2018-09-24 14:07:51 +0200

    Added informations into the previewView page

--- documentroot/sources/controller/previewController.php
+++ documentroot/sources/controller/previewController.php
@@ -6,5 +6,12 @@
  * Time: 15:53
  */
 
+require_once ("sources/model/artists.php");
+
+$artists = getArtists();
+$artistId = (int)$_GET['id'];
+$actualArtist = $artists[$artistId];
+$pathToArtistsImage = pathToArtistsImages() . $actualArtist['imageurl'];
+
 require_once ("sources/view/previewView.html");
 ?>
