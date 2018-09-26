52b286c0237ec48d3d4dc9c5da098eb6c5ec9f83
MisterX25 <Xavier.Carrel@cpnv.ch>
2018-09-26 12:14:22 +0200

    Db integration finished
    Includes performance date/times

--- documentroot/sources/model/artists.php
+++ documentroot/sources/model/artists.php
@@ -6,31 +6,33 @@
  * Time: 16:35
  */
 
+require_once("database.php"); // for db connection
+
+// Returns the whole lineup as an array of associative arrays
 function getArtists()
 {
-    $host = 'localhost';
-    $db   = 'lineup';
-    $user = 'root';
-    $pass = 'root';
-    $charset = 'utf8mb4';
+    $pdo = dbConnection();
 
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-    $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-    ];
-    try {
-        $pdo = new PDO($dsn, $user, $pass, $options);
-    } catch (\PDOException $e) {
-        throw new \PDOException($e->getMessage(), (int)$e->getCode());
-    }
-
-    $stmt = $pdo->prepare('SELECT Name, Gender_id as kind FROM Artists;');
+    $stmt = $pdo->prepare(
+        'select Artists.id, Artists.Name as name, Mainpicture as mainpicture, Description as description, G.Name as kind, C.Name as country 
+         from Artists inner join Countries C on Artists.Country_id = C.id inner join Genders G on Artists.Gender_id = G.id');
     $stmt->execute();
     $artists = $stmt->fetchAll();
-    error_log(print_r($artists,1));
+    foreach ($artists as $key => $artist)
+        $artists[$key]['performances'] = getArtistPerformances($pdo,$artist['id']);
     return $artists;
+}
+
+// Returns the list of performances of a given artist
+// db connection is passed a parameter in order to avoid reconnecting (dependency injection)
+function getArtistPerformances($pdo,$aid)
+{
+    $stmt = $pdo->prepare(
+        'select Date_time as datetime, Duration as duration, S.Name as scene
+        from PerformanceDates inner join Scenes S on PerformanceDates.Scene_id = S.id
+        where Artist_id = :aid;');
+    $stmt->execute(['aid' => $aid]);
+    return $stmt->fetchAll();
 }
 
 ?>
