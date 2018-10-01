bc7260ebfe42188d415ffe97da42babc569c16a6
ILJAZI Dardan <Dardan.ILJAZI@cpnv.ch>
2018-10-01 14:24:58 +0200

    Add PerformanceDates class

---
+++ documentroot/sources/model/PerformanceDates.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dardan.Iljazi
+ * Date: 01.10.2018
+ * Time: 14:21
+ */
+
+class PerformanceDates
+{
+    public function __construct()
+    {
+    }
+}
