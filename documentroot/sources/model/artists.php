cd348a0e049395e4ed841438fb3f2a57b236971b
DavidNiembro <david@niembro.ch>
2018-10-01 14:33:05 +0200

    Add PDO

--- documentroot/sources/model/artists.php
+++ documentroot/sources/model/artists.php
@@ -5,21 +5,60 @@
  * Date: 02.09.18
  * Time: 16:35
  */
+require_once "vendor/autoload.php";
+$dotenv = new Dotenv\Dotenv($_SERVER['DOCUMENT_ROOT']);
+$dotenv->load();
+
+function connexion(){
+    $host = getenv("SERVER");
+    $db = getenv("DB");
+    $user = getenv("USER");
+    $pass = getenv("PASSWORD");
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+    return $pdo;
+}
 
 function getArtists()
 {
-    return [
-        ["id"=>"0","name" => "Kaleo", "kind" => "Blues Rock", "image"=>"kaleo.jpg", "description"=>"D’un côté, l’Islande et ses 300'000 habitants. De l’autre, les États-Unis, pays de la démesure. Et entre les deux, Kaleo. Ces quatre amis d’enfance, originaires des environs de Reykjavik, ont plié bagage pour s’installer au Texas. Entre rock, folk et blues, ils puisent dans l’atmosphère mystique des fjords et dans la chaleur moite des déserts. Leur album A/B révèle leur dualité: entre titres racés et ballades gracieuses, leur cœur balance. Mais il faudrait bien plus que deux faces à cet opus pour illustrer la virtuosité du groupe, qui jongle tant avec les riffs rocailleux ou les aurores boréales qu’avec la poussière d’Austin."],
-        ["id"=>"1","name" => "Txarango", "kind" => "Latino", "image"=>"txarango.jpg", "description"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."],
-        ["id"=>"2","name" => "Nekfeu", "kind" => "Rap", "image"=>"nekfeu.jpg", "description"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]
-    ];
+    $pdo = connexion();
+
+    $stmt = $pdo->prepare('SELECT * FROM Artists ');
+    $stmt->execute();
+    return $stmt->fetchAll();
 }
+
 function getArtistsByID($id){
-    $artistes = getArtists();
-    foreach ($artistes as $artiste){
-        if($artiste['id']==$id){
-            return $artiste;
-        }
-    }
+    $pdo = connexion();
+
+    $stmt = $pdo->prepare('SELECT * FROM Artists where id = :id ');
+    $stmt->execute(['id'=>$id]);
+    $artist = $stmt->fetch();
+
+    $performance = getPerformance($id);
+    $artist['performance'] = $performance;
+    error_log(print_r($artist,1));
+    return $artist;
+}
+
+function getPerformance($id)
+{
+    $pdo = connexion();
+
+    $stmt = $pdo->prepare('SELECT * FROM PerformanceDates where Artist_id = :artistID ');
+    $stmt->execute(['artistID'=>$id]);
+    return $stmt->fetchALL();
 }
 ?>
