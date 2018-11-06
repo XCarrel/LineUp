0053d44d9cfba3a4b0a70cec4f05148565ad4f77
Senistan <senistan.jegarajasingam@cpnv.ch>
2018-11-06 08:02:00 +0100

    AJAX

--- documentroot/sources/js/main.js
+++ documentroot/sources/js/main.js
@@ -1,3 +1,30 @@
 $(document).ready(function (){
-    console.log('test')
+    // hide save button
+    $('#savebutton').hide();
+
+    //Show button when a value has changed
+    $('#countrybox').change(function () {
+        $('#savebutton').show();
+    });
+
+    $('#genderslists').change(function () {
+       $('#savebutton').show();
+    });
+
+    $('#description').change(function () {
+        $('#savebutton').show();
+    });
+
+    //Send new data on click
+    $("#savebutton").click(function () {
+        $.ajax({
+            method: 'POST',
+
+            data: {'gender':$('#genderslists option:selected').val()},
+            'country':$(#countrylists).val(),
+            success: function (data) {
+                $('#savebutton').hide();
+            }    
+        });
+    });
 })
