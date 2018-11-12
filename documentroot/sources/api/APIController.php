187943567c69687fc22d530259c3261a82973f15
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-11-12 15:22:19 +0100

    Page qui envoie les requête à la base de données

--- documentroot/sources/api/APIController.php
+++ documentroot/sources/api/APIController.php
@@ -9,13 +9,38 @@
 extract ($_POST); // $artistid, $description, $countryid, $genderid
 
 require_once ("sources/model/Artist.php");
+require_once ("sources/model/Gender.php");
 
+error_log((print_r($_POST, true)));
+
+$gender = new Gender();
+if($_POST['gender'] != "")
+{
+    $gender->setName($_POST['gender']);
+    $gender->create();
+}
+else if($_POST['genderDel'] != "")
+{
+    foreach($_POST['genderDel'] as $genderDel)
+    {
+        $gender->setId($genderDel);
+        $gender->destroy();
+    }
+}
+else if($_POST['id'] != "" && $_POST['genderUpdate'] != "")
+{
+    $gender->setId($_POST['id']);
+    $gender->setName($_POST['genderUpdate']);
+    $gender->store();
+}
+
+/*
 $artist = new Artist();
 $artist->load($artistid);
 $artist->setDescription($description);
 $artist->setCountryId($countryid);
 $artist->setGenderId($genderid);
 $artist->store();
-
+*/
 ?>
 
