6c75bf41ecb460acd59a6e4281978623ab5aec5d
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-11-12 15:21:07 +0100

    Montrer au professeurs que c'est mon site

--- documentroot/index.php
+++ documentroot/index.php
@@ -12,7 +12,7 @@ require_once "sources/unittest/UnitTests.php";
 UnitTests::testPersistableOnArtist();
 
 //*/
-$appVersion = "v1.0";
+$appVersion = "Examen de Jérémy";
 
 $page = isset($_GET["page"]) ? $_GET["page"] : "home";
 
