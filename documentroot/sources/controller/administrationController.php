14d335eeb0d807c251ca451eeb1a8229e664e794
QuentinRo <quentin.rossier@cpnv.ch>
2018-11-12 16:11:22 +0100

    Update documentroot/sources/controller/administrationController.php

---
+++ documentroot/sources/controller/administrationController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Xavier
+ * Date: 02.09.18
+ * Time: 16:42
+ */
+
+require_once ("sources/model/Gender.php");
+
+$genders = Gender::All();
+
+require_once ("sources/view/administrationView.html");
