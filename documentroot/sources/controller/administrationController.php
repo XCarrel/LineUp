eed7d36167ab0f2df6376073bd38afe4853798ff
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-11-20 09:43:10 +0100

    Ajout du post pour mes données

--- documentroot/sources/controller/administrationController.php
+++ documentroot/sources/controller/administrationController.php
@@ -6,8 +6,35 @@
  * Time: 16:42
  */
 
-require_once ("sources/model/Artist.php");
+require_once ("sources/model/Database.php");
+require_once ("sources/model/iPersistable.php");
 require_once ("sources/model/Gender.php");
+
+error_log(print_r($_POST, true));
+extract($_POST);
+
+if (isset($add)) // New gender inserted
+{
+    $genderAdd = new Gender();
+    $genderAdd->setName($addtext);
+    $genderAdd->create();
+}
+if (isset($delete))
+{
+    foreach ($genders as $genderID)
+    {
+        $genderDelete = new Gender();
+        $genderDelete->load($genderID);
+        $genderDelete->destroy();
+    }
+}
+if (isset($update))
+{
+    $genderUpdate = new Gender();
+    $genderUpdate->setId($genders[0]);
+    $genderUpdate->setName($updatetext);
+    $genderUpdate->store();
+}
 
 // Reprendre tous les genres dans la base de données
 $genders = Gender::All();
