8cd3f75be1266d73d9c5c4208540574ec6747fa8
JUNOD Alexandre <Alexandre.JUNOD@cpnv.ch>
2018-11-20 10:21:52 +0100

    Amélioration interface utilisateur

--- documentroot/sources/js/administrationView.js
+++ documentroot/sources/js/administrationView.js
@@ -6,20 +6,31 @@
 
 $(document).ready(function () {
 
-    // Make the "add new gender" button appear when a change is made in the page
-    $('#InputGender').keypress(function () {
-        touchInput();
-    })
-
-    //Make appear the other buttons while checked a check box
-    $('input:checkbox').change(function () {
-        touchCheckbox();
-    })
-
-    //Detect when the user press on the keyboard, detect delete and back
-    $('#InputGender').on('keydown', function () {
+   // Make the "add new gender" button appear when a change is made in the page
+   $('#InputGender').keypress(function () {
       touchInput();
-    })
+   })
+
+   // The user press on the keyboard, detect delete and backspace works
+   $('#InputGender').on('keydown', function () {
+      touchInput();
+   })
+
+   // Make appear the other buttons while checked a check box
+   $('input:checkbox').change(function () {
+      touchCheckbox();
+   })
+
+   // Change the button "Renommer le sélectionné en" when a key is pressed
+   $('#RenameGender').keypress(function () {
+      touchInputRename();
+   })
+
+   // Change the button "Renommer le sélectionné en" when a key is pressed, delete and backspace works
+   $('#RenameGender').on('keydown', function () {
+      touchInputRename();
+   })
+
 })
 
 // Mark the page as dirty, i.e: changes were made
@@ -33,6 +44,19 @@ function touchInput() { //Doesn't work 100% well, it let the user put only 1 let
     {
         $('#cmdNewGender').addClass('disabled'); //Add the class disabled
         $('#cmdNewGender').attr('disabled', true); //Disable the button to prevent to send
+    }
+}
+
+function touchInputRename() { //Doesn't work 100% well, it let the user put only 1 letter
+    if ($('#RenameGender').val().length >=2)
+    {
+        $('#cmdRenameGender').removeClass('disabled'); //Remove the class "disabled"
+        $('#cmdRenameGender').attr('disabled', false); //Put the button on enable
+    }
+    else
+    {
+        $('#cmdRenameGender').addClass('disabled'); //Add the class disabled
+        $('#cmdRenameGender').attr('disabled', true); //Disable the button to prevent to send
     }
 }
 
